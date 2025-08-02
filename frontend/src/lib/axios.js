473c572003883ac0ff71bbc4e307a527c5643bf4
KAMLESH <131372442+KAMLESH7939@users.noreply.github.com>
2025-08-02 06:56:57 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -7,7 +7,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://inclusight-production.up.railway.app/api",
+      : import.meta.env.VITE_BACKEND_URL || "https://inclusight-production.up.railway.app/api",
   withCredentials: true,
 });
 
